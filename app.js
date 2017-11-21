91cd808e9a012739cbd9fe23cd2fa65899ece53d
Headwhate <t.wiart@outlook.fr>
2017-11-22 00:56:18 +0100

    Header done

---
+++ app.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+  // navbar
+  $('.burger').click(function(){
+    $(this).find(".barre").toggleClass('white')
+    $('.nav').toggleClass('isOpen');
+  
+  });
+
+  $('.principaux li:first-child').hover(function(){
+
+    $('.pfirst').toggleClass('show');
+  });
+$('.principaux li:nth-child(2)').hover(function(){
+
+  $('.psecond').toggleClass('show');
+});
+$('.principaux li:last-child').hover(function(){
+
+  $('.pthird').toggleClass('show');
+});
+
+$('.secondaires li:first-child').hover(function(){
+
+  $('.sfirst').toggleClass('show');
+});
+
+$('.secondaires li:nth-child(2)').hover(function(){
+
+  $('.ssecond').toggleClass('show');
+});
+$('.secondaires li:last-child').hover(function(){
+
+  $('.sthird').toggleClass('show');
+});
+// fin navbar
+});
